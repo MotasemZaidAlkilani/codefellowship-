f4a94ce284ac667ae53494fb0a07713889e3005c
motasemZaidAlkilani <m.kilany1999@gmail.com>
2022-03-30 10:25:17 +0300

    initial comment

---
+++ src/main/java/com/example/codeFellows/CodeFellowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeFellows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeFellows/CodeFellowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeFellows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
